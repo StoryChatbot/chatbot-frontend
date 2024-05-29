918de690e7fdc7024618f197d875a1458db11f92
Aya Hamza <117121490+aaiaa-1@users.noreply.github.com>
2024-05-29 19:50:20 +0100

    added chat logic

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -12,16 +12,32 @@ import { CommonModule } from '@angular/common';
 })
 export class ChatComponent {
 
-  prompt!: string;
-  story!: string;
+  messages = [
+    { sender: 'user', text: 'Hello!' },
+    { sender: 'chatbot', text: 'Hi there!' },
+  ];
+  prompt: string = '';
+
+
+  story: string = '';
 
   constructor(private storyService: GenerateStoryService) { }
 
+
   generateStory() {
     console.log(this.prompt);
+    
+    if (this.prompt) {
+      this.messages.push({ sender: 'user', text: this.prompt });
+    }
+
     this.storyService.generateStory(this.prompt).subscribe(response => {
       this.story = response.story;
     });
+
+
+    this.prompt = '';
+
   }
 
 }
